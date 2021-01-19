b248d064c6144f029f345105425e90307ff22aec
saddamarbaa <saddamarbaas@gmail.com>
2021-01-20 04:39:29 +0700

    ADDDED: fetchMovies() function to fetch data from IMDB API

--- script.js
+++ script.js
@@ -10,34 +10,20 @@ const myListMovies = document.querySelector(".myList__movies");
 // console.log(topRatedMovies);
 // console.log(myListMovies);
 
-// let img1 = `<img
-// src="https://occ-0-64-58.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABTNtbTme6kjhQ5E_4UG-98Aar9knOv6iGoVle_WQ8Ik26Zd9QIhXworg3IbCij348hy9E9RqvXI0TVS7NaKBQ6NcOXCY_nc8Snk4ob4QpGCX5vRNXR1ZdwFHlYC1.jpg?r=909"
-// alt="original__movies"
-// ></img>`;
-
-// let img2 = `  <img
-// src="https://occ-0-64-58.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABdl4tSD3mYVstPPRTUw66gYsDWSFEjb0zidksP4exUcGNQ2KHSKuxyV4wzJQFqvh2EVT-ay3p__njM6smgfCtTYylCM.jpg?r=4e5"
-// alt="original__movies">`;
-
-// let img3 = `<img src="https://cdn57.androidauthority.net/wp-content/uploads/2019/12/tall-girl-movie-image.jpg"
-// alt="original__movies">`;
-
-// let img4 = ` <img
-// src="https://occ-0-64-58.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABREjGfvO2NFXWWJEUI_SYKs95sva-C80Rip6CcllHUm4Jbvm-8-i5VHtchn59Wrq1mr2MmC3hoF7nzB6sUX94YSwdqE.jpg?r=747"
-// alt="original__movies">`;
-
 //  Event Listener and Functions
 
 //  Add Movies to the front end
 
 function addMovies(movies, moveEl) {
-  console.log(movies);
-  for (const movie of movies.results) {
-    const img = `<img src=https://image.tmdb.org/t/p/original${movie.backdrop_path}>`;
+  // console.log(movies);
+  // console.log(movie.backdrop_path);
 
-    moveEl.innerHTML += img;
-    console.log(img);
-  }
+  movies.forEach((movie) => {
+    const image = `<img src=https://image.tmdb.org/t/p/original${movie.backdrop_path} alt = "img" >`;
+
+    // console.log(image);
+    moveEl.innerHTML += image;
+  });
 }
 
 function fetchMovies() {
@@ -49,11 +35,16 @@ function fetchMovies() {
         return response.json();
       } else {
         // throw new Error(response.statusText);
-        throw new Error("Something went wrong ");
+        throw new Error("Something went wrong");
       }
     })
     .then((data) => {
-      console.log(data);
+      // console.log(data);
+      // call add function and pass movies arra
+      addMovies(data.results, tredingNowMovies);
+      addMovies(data.results, originalMovies);
+      addMovies(data.results, topRatedMovies);
+      addMovies(data.results, myListMovies);
     })
     .catch((error) => {
       console.log("Fetch Error :-S", error);
@@ -64,9 +55,4 @@ function fetchMovies() {
 
 window.onload = () => {
   fetchMovies();
-  fetchMovies();
-  // addMovies(originalMovies, img1);
-  // addMovies(tredingNowMovies, img2);
-  // addMovies(topRatedMovies, img3);
-  // addMovies(myListMovies, img4);
 };
