093026152d5cbbb982d592d90d45cfa306cada57
saddamarbaa <saddamarbaas@gmail.com>
2021-01-20 12:15:06 +0700

    CODE REVIEW: updated fetchMovies() function to have only one fetchMovies() with parameters

--- script.js
+++ script.js
@@ -1,140 +1,72 @@
-const myListMovies = document.querySelector(".myList__movies");
+// Selectors
+
+// fetch movies data from (TMDb) API
+
+function fetchMovies(url, element_selector, path_type) {
+  fetch(url)
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        // throw new Error(response.statusText);
+        throw new Error("Something went wrong");
+      }
+    })
+    .then((data) => {
+      // console.log(data);
+      showMovies(data.results, element_selector, path_type);
+    })
+    .catch((error) => {
+      console.log("Fetch Error :-S", error);
+    });
+}
 
 //  Event Listener and Functions
 
 //  Add Movies to the front end
 
-function addMovies(movies) {
-  // Selectors
-  const originalMovies = document.querySelector(".original__movies");
+function showMovies(movies, element_selector, path_type) {
   // console.log(movies);
-  // console.log(movie.backdrop_path);
-  // console.log(originalMovies);
+  // console.log(element_selector);
+  // console.log(path_type);
 
+  const moviesEl = document.querySelector(element_selector);
   movies.forEach((movie) => {
-    const image = `<img src=https://image.tmdb.org/t/p/original${movie.poster_path} alt = "img" >`;
+    const image = `<img src=https://image.tmdb.org/t/p/original${movie[path_type]} alt = "img" >`;
 
     // console.log(image);
-    originalMovies.innerHTML += image;
+    moviesEl.innerHTML += image;
   });
 }
 
-function showTredingNowMovie(movies) {
-  //  Selector
-  const tredingNowMovies = document.querySelector(".TredingNow__movies");
-  // console.log(movies);
-  // console.log(movie.backdrop_path);
-
-  movies.forEach((movie) => {
-    const image = `<img src=https://image.tmdb.org/t/p/original${movie.backdrop_path} alt = "img" >`;
-
-    // console.log(image);
-    tredingNowMovies.innerHTML += image;
-  });
+function getOriginalsMovies() {
+  const url =
+    "https://api.themoviedb.org/3/discover/tv?api_key=19f84e11932abbc79e6d83f82d6d1045&with_networks=213";
+  fetchMovies(url, ".original__movies", "poster_path");
 }
 
-function showTopRatedMovies(movies) {
-  //  Selectors
-  const topRatedMovies = document.querySelector(".topRated__movies");
-  // console.log(movies);
-  // console.log(movie.backdrop_path);
-
-  movies.forEach((movie) => {
-    const image = `<img src=https://image.tmdb.org/t/p/original${movie.backdrop_path} alt = "img" >`;
-
-    // console.log(image);
-    topRatedMovies.innerHTML += image;
-  });
-}
-
-function fetchMovies() {
-  fetch(
-    "https://api.themoviedb.org/3/discover/tv?api_key=19f84e11932abbc79e6d83f82d6d1045&with_networks=213"
-  )
-    .then((response) => {
-      if (response.ok) {
-        return response.json();
-      } else {
-        // throw new Error(response.statusText);
-        throw new Error("Something went wrong");
-      }
-    })
-    .then((data) => {
-      // console.log(data);
-      addMovies(data.results);
-    })
-    .catch((error) => {
-      console.log("Fetch Error :-S", error);
-    });
-}
-
-function getTredingNowMovies() {
-  fetch(
-    "https://api.themoviedb.org/3/trending/movie/week?api_key=19f84e11932abbc79e6d83f82d6d1045&with_networks=213"
-  )
-    .then((response) => {
-      if (response.ok) {
-        return response.json();
-      } else {
-        // throw new Error(response.statusText);
-        throw new Error("Something went wrong");
-      }
-    })
-    .then((data) => {
-      // console.log(data);
-      showTredingNowMovie(data.results);
-    })
-    .catch((error) => {
-      console.log("Fetch Error :-S", error);
-    });
+function getTrendingNowMovies() {
+  const url =
+    "https://api.themoviedb.org/3/trending/movie/week?api_key=19f84e11932abbc79e6d83f82d6d1045";
+  fetchMovies(url, ".TredingNow__movies", "backdrop_path");
 }
 
 function getTopRatedMovies() {
-  fetch(
-    "https://api.themoviedb.org/3/movie/top_rated?api_key=19f84e11932abbc79e6d83f82d6d1045&language=en-US&page=1"
-  )
-    .then((response) => {
-      if (response.ok) {
-        return response.json();
-      } else {
-        // throw new Error(response.statusText);
-        throw new Error("Something went wrong");
-      }
-    })
-    .then((data) => {
-      // console.log(data);
-      showTopRatedMovies(data.results);
-    })
-    .catch((error) => {
-      console.log("Fetch Error :-S", error);
-    });
+  const url =
+    "https://api.themoviedb.org/3/movie/top_rated?api_key=19f84e11932abbc79e6d83f82d6d1045&language=en-US&page=1";
+  fetchMovies(url, ".topRated__movies", "backdrop_path");
 }
 
-function getTredingNowMovies() {
-  fetch(
-    "https://api.themoviedb.org/3/trending/movie/week?api_key=19f84e11932abbc79e6d83f82d6d1045&with_networks=213"
-  )
-    .then((response) => {
-      if (response.ok) {
-        return response.json();
-      } else {
-        // throw new Error(response.statusText);
-        throw new Error("Something went wrong");
-      }
-    })
-    .then((data) => {
-      // console.log(data);
-      showTredingNowMovie(data.results);
-    })
-    .catch((error) => {
-      console.log("Fetch Error :-S", error);
-    });
-}
+// function getMyListMovies() {
+// 	const url =
+// 			"https://api.themoviedb.org/3/movie/top_rated?api_key=19f84e11932abbc79e6d83f82d6d1045&language=en-US&page=1";
+// 	fetchMovies(url, ".myList__movies", "backdrop_path");
+// }
 
 // function call
 
 window.onload = () => {
-  fetchMovies();
-  getTredingNowMovies();
+  getOriginalsMovies();
+  getTrendingNowMovies();
   getTopRatedMovies();
 };
