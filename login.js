a060ef3737acc2618867a736c347015d6f47dbd8
saddamarbaa <saddamarbaasaddamarbaa@gmail.com>
2021-01-30 09:23:43 +0700

    ADD:Login form

---
+++ login.js
@@ -0,0 +1,10 @@
+// Selecters
+
+const loginForm = document.getElementById("loginForm");
+// console.log(loginForm);
+
+loginForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  // console.log(e);
+  // console.log("submited");
+});
